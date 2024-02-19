cdc893860b2b292d45e4307224ab2837538d9114
main <gari-vlad-2005@yandex.ru>
2024-02-19 16:16:20 +0900

    its finaly fucking working well

--- entity.py
+++ entity.py
@@ -48,10 +48,6 @@ class Entity(pygame.sprite.Sprite):
         if self.life:
             self.sprite_magic(list_sprites)
             self.hand_controll()
-        pygame.draw.rect(self.surface, "WHITE", self.left_detect)
-        pygame.draw.rect(self.surface, "WHITE", self.right_detect)
-        pygame.draw.rect(self.surface, "GREEN", self.bottom_detect)
-        pygame.draw.rect(self.surface, "GREEN", self.top_detect)
         self.rect.x += self.x_speed
         self.rect.y += self.y_speed
 
@@ -60,29 +56,32 @@ class Entity(pygame.sprite.Sprite):
                                                self.rect[2] // 4, self.rect[3] - 20))
         self.right_detect = Heatbox(pygame.Rect(self.rect.x + self.rect[2] // 4 * 3, self.rect.y + 10,
                                                 self.rect[2] // 4, self.rect[3] - 20))
-        self.bottom_detect = Heatbox(pygame.Rect(self.rect.bottomleft[0] + 10, self.rect.bottomleft[1] - 6,
+        self.bottom_detect = Heatbox(pygame.Rect(self.rect.bottomleft[0] + 10, self.rect.bottomleft[1] - 4,
                                                  self.rect[2] - 20, 5))
         self.top_detect = Heatbox(pygame.Rect(self.rect.x + 10, self.rect.y + 1, self.rect[2] - 20, 5))
         if len(pygame.sprite.spritecollide(self, list_sprites, dokill=False)) > 0:
             for i in pygame.sprite.spritecollide(self, list_sprites, dokill=False):
                 sprite = i
                 if pygame.sprite.spritecollide(self.bottom_detect, list_sprites, dokill=False):
-                    self.rect.bottom = sprite.rect.top
+                    if sprite.rect.top - self.rect.bottom >= 5:
+                        self.rect.bottom = sprite.rect.top
                     self.y_speed = 0
                     self.grounded = True
                 if pygame.sprite.spritecollide(self.top_detect, list_sprites, dokill=False):
                     self.y_speed = self.gravity
                     self.rect.top = sprite.rect.bottom
                 if pygame.sprite.spritecollide(self.right_detect, list_sprites, dokill=False):
-                    if self.x_speed > 0:
-                        self.x_speed = 0
+                    self.x_speed = 0
                     self.rect.right = sprite.rect.left
                 if pygame.sprite.spritecollide(self.left_detect, list_sprites, dokill=False):
-                    if self.x_speed < 0:
-                        self.x_speed = 0
+                    self.x_speed = 0
                     self.rect.left = sprite.rect.right
         else:
             self.grounded = False
 
     def draw(self):
         self.surface.blit(self.image, self.rect)
+        pygame.draw.rect(self.surface, "WHITE", self.left_detect)
+        pygame.draw.rect(self.surface, "WHITE", self.right_detect)
+        pygame.draw.rect(self.surface, "GREEN", self.bottom_detect)
+        pygame.draw.rect(self.surface, "GREEN", self.top_detect)
